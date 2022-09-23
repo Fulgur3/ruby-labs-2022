65354e80915b6ed15685e364718ce56c5c0ce2b5
Fulgur3 <belokrsasha@gmail.com>
2022-09-23 12:45:58 +0300

    3rd lab fix

--- 3rd lab/3rd part.rb
+++ 3rd lab/3rd part.rb
@@ -3,7 +3,7 @@ def func3(x)
   n=gets.chomp.to_i
   sum=0
   fact=1
-  (o..n+1).each {
+  (0..n+1).each {
     |i|
     sum+=(x.to_f**i)/fact
     fact*=(i+1)
@@ -21,6 +21,9 @@ def func5(x)
   return Math.sqrt(2.0 + multiple_sqrt(n - 1))
   end
 
+def multiple_sqrt(n)
+  return n == 0 ? 0 : Math.sqrt(2.0 + multiple_sqrt(n - 1))
+end
 
-puts "Sum 1 + x^1/1! + ... + x^n/n! = #{func3(x)}"
-puts "Sum of n variables in sqrt(2+sqrt(2+...)=#{func5(x)}"
+puts "Sum 1 + x^1/1! + ... + x^n/n! = #{func3(3)}"
+puts "Sum of n variables in sqrt(2+sqrt(2+...)=#{func5(5)}"
